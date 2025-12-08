8fcc9f03c24ae3e36627ff58b84dcc2f8cdd8c7a
Jonathan Allotey <alloteyj@roverkids.org>
2025-12-08 14:05:48 -0500

    2.1 Complete
    once 2.1 was completed

---
+++ AnimalStampede/BasicGameplay/Assets/Course Library/_Source_Files/Scripts/PlayerController.cs
@@ -0,0 +1,32 @@
+using UnityEngine;
+
+public class NewMonoBehaviourScript : MonoBehaviour
+
+{
+    public float speed = 10.0f;
+    public float horizontalInput;
+    public float xRange = 10.0f;   // Start is called once before the first execution of Update after the MonoBehaviour is created
+    void Start()
+    {
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {// Keeps The Player Inbounds.
+        if (transform.position.x < xRange)
+        {
+            transform.position = new Vector3(xRange, transform.position.y, transform.position.z);
+
+
+        }if (transform.position.x > xRange)
+
+            transform.position = new Vector3(xRange, transform.position.y, transform.position.z);
+        {
+
+            horizontalInput = Input.GetAxis("Horizontal");
+            transform.Translate(Vector3.right * horizontalInput * Time.deltaTime * speed);
+        }
+    }
+}        
+      
